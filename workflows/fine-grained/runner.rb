9b45c533e2a3801fd9bda205e1ba9f964cbb70c4
Kamil Figiela <kamil.figiela@gmail.com>
2014-05-07 15:06:50 +0200

    Add fine-grained model

---
+++ workflows/fine-grained/runner.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require_relative 'workflow_problem'
+
+begin 
+  Dir.mkdir('outs')
+rescue
+  # Ignore already if exists
+end
+
+problems = []
+60.downto(10).each do |deadline|
+  run_optimization!("workflow_deadline" => deadline/60.0, "storage" => "S3")
+  # run_optimization!("workflow_deadline" => deadline, "storage" => "CloudFiles")
+  # pbs_optimization!("test_pbs", "workflow_deadline" => deadline, "storage" => "S3")
+  # pbs_optimization!("test_pbs", "workflow_deadline" => deadline, "storage" => "CloudFiles")
+end
---
+++ workflows/fine-grained/workflow_problem.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+require 'yaml'
+require 'cmpl'
+
+module YAML
+  def self.dump_file(data, filename)
+    File.open(filename, 'w') do |out|
+      YAML.dump(data, out)
+    end
+  end
+end
+
+def schema 
+  {
+      instances: {
+        instance_price: Numeric,
+        cores: Numeric,
+      },
+      providers: {
+        provider_max_machines: Numeric,
+        transfer_price_in: Numeric,
+        transfer_price_out: Numeric,
+        instances: DataGenerator::Tuple.new([:instances])
+      },
+      storages: {
+        storages_transfer_price_in: Numeric,
+        storages_transfer_price_out: Numeric
+      },
+      transfer_rate: DataGenerator::Vector.new([:storages, :providers]),
+      transfer_latency: DataGenerator::Vector.new([:storages, :providers]),
+      request_price: Numeric,
+      storage_local_rel: DataGenerator::Tuple.new([:storages, :providers]),
+      tasks: {
+        task_count: Numeric,
+        data_size_in: Numeric,
+        data_size_out: Numeric
+      },
+      exec_time: DataGenerator::Vector.new([:tasks, :instances]),
+      layers: { 
+        tasks: DataGenerator::Tuple.new([:tasks])
+      },
+      workflow_deadline: Numeric,
+      storage: Symbol
+  }
+end
+
+def run_optimization!(params)
+  problem = Problem.new schema, debug: true
+
+  infrastructure = YAML.load_file('infrastructure.yaml')
+  workflow = YAML.load_file('montage.8.0.yaml')
+
+  problem.params = infrastructure.merge(workflow).merge(params)
+
+  solution = problem.run!
+  result = {solution: solution, params: params}
+  YAML.dump_file(result, "outs/#{params.values.join("_").downcase}.yaml")
+  File.open("out.txt", 'a') do |out|
+    out.puts "%s    %s" % [params.values.map{|v| "%20s" % [v] }.join("\t"), solution.objective.value.inspect]
+  end
+  return result
+end
+
+def pbs_optimization!(dir, params)
+  problem = Problem.new schema, debug: true
+
+  infrastructure = YAML.load_file('infrastructure.yaml')
+  workflow = YAML.load_file('workflow.yaml')
+
+  problem.params = infrastructure.merge(workflow).merge(params)
+
+  path = dir+"/#{params.values.join("_").downcase}"
+  problem.generate_offline_problem(path)
+  
+  path + "/run.sh"
+end
